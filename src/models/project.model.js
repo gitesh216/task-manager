52d37c9896a8b605dfee5bba289bd06f5495e375
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-16 22:54:14 +0530

    update Project schema to support multiple members with roles

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -1,23 +1,36 @@
 import mongoose, { Schema } from "mongoose";
 
 const projectSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        members: [
+            {
+                user: {
+                    type: Schema.Types.ObjectId,
+                    ref: "User",
+                },
+                role: {
+                    type: String,
+                    enum: ["admin", "editor", "viewer"],
+                    default: "viewer",
+                },
+            },
+        ],
     },
-    description: {
-      type: String,
-    },
-    createdBy: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
 
 export const Project = mongoose.model("Project", projectSchema);
