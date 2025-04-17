d4acacbc1a6011305cc83bbb23aba8d5510a51c5
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-17 19:51:24 +0530

    update subtask model schema

--- src/models/subtask.model.js
+++ src/models/subtask.model.js
@@ -1,7 +1,25 @@
 import mongoose, { Schema } from "mongoose";
 
-const subTaskSchema = new Schema({
+const subtaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    task: {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {timestamps: true});
 
-})
-
-export const SubTask = mongoose.model("SubTask", subTaskSchema);
+export const SubTask = mongoose.model("SubTask", subtaskSchema);
