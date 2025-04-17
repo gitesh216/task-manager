bb7f83e4293dd64424dd4d1cd3ff6a88a8049ec9
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-04-17 11:13:21 +0530

    update project model schema

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -16,19 +16,6 @@ const projectSchema = new Schema(
             ref: "User",
             required: true,
         },
-        members: [
-            {
-                user: {
-                    type: Schema.Types.ObjectId,
-                    ref: "User",
-                },
-                role: {
-                    type: String,
-                    enum: ["admin", "editor", "viewer"],
-                    default: "viewer",
-                },
-            },
-        ],
     },
     { timestamps: true },
 );
