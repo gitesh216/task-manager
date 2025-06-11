4f39be861dea6d4fa1403a3439d6592a5ce83977
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-06-11 14:18:36 +0530

    feat: add create task controller

--- src/controllers/task.controllers.js
+++ src/controllers/task.controllers.js
@@ -1,16 +1,45 @@
-// get all tasks
+import { ProjectMember } from "../models/projectmember.model.js"
+import { Project } from "../models/project.model.js";
+import { asyncHandler } from "../utils/async-handler.js"
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import User from "../models/user.model.js"
+import { Task } from "../models/task.model.js";
+
 const getTasks = async (req, res) => {
     // get all tasks
 };
 
 // get task by id
 const getTaskById = async (req, res) => {
-    // get task by id
+    
 };
 
 // create task
 const createTask = async (req, res) => {
-    // create task
+    const { title, description, assignedTo, status } = req.body;
+    const { projectId } = req.params;
+    if(!title || !description || !assignedTo || status || projectId){
+        throw new ApiError(400, "All task details required")
+    }
+    const assignedUser = await User.findById(assignedTo)
+    if(!assignedUser){
+        throw new ApiError(400, "Assigned user not found")
+    }
+    const task = await Task.create({
+        title,
+        description,
+        project: projectId,
+        assignedTo: assignedUser._id,
+        assignedBy: req.user._id,
+        status
+    });
+    if(!task){
+        throw new ApiError(501, "Error in creating new task")
+    }
+    return res.status(200).json(
+        new ApiResponse(200, task, "Task created successfully")
+    )
 };
 
 // update task
