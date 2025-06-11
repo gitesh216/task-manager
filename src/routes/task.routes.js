779151aa13acfd4f048b7dac25aff4937265695e
gitesh216 <109015479+gitesh216@users.noreply.github.com>
2025-06-11 15:10:08 +0530

    feat: add task routes

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -16,5 +16,19 @@ const router = Router();
 
 router.route("/create-task/:projectId").post(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), createTask);
 
+router.route("/updateTask/:taskId").post(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), updateTask);
+
+router.route("/create-subtask/:taskId").post(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), createSubTask);
+
+router.route("/update-subtask/:subtaskId").post(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), updateSubTask);
+
+router.route("/get-tasks/:projectId").get(isLoggedIn, getTasks);
+
+router.route("/get-task/:taskId").get(isLoggedIn, getTaskById);
+
+router.route("/delete-task/:taskId").get(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), deleteTask);
+
+router.route("/delete-subtask/:subtaskId").get(isLoggedIn, validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER, UserRolesEnum.PROJECT_ADMIN]), deleteSubTask);
+
 export default router;
 
